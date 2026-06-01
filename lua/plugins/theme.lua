6bcff30073c6ae217dd6d75fba0762e7cb689268
Don Freecs <freecsdon30@gmail.com>
2026-06-01 16:56:02 +0530

    feat: add github dark theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,28 +1,16 @@
 return {
 	{
-		url = "https://codeberg.org/evergarden/nvim.git",
-		name = "evergarden",
+		url = "https://github.com/projekt0n/github-nvim-theme",
+		name = "github-theme",
 		priority = 1000,
 		config = function()
-			require("evergarden").setup({
-				theme = {
-					variant = "winter",
-					accent = "orange",
-				},
-				editor = {
-					transparent_background = false,
-					sign = { color = "none" },
-					float = {
-						color = "mantle",
-						solid_border = false,
-					},
-					completion = {
-						color = "surface0",
-					},
+			require("github-theme").setup({
+				options = {
+					transparent = false,
 				},
 			})
 
-			vim.cmd("colorscheme evergarden")
+			vim.cmd("colorscheme github_dark")
 		end,
 	},
 }
